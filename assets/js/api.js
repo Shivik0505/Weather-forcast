b93b999587e39ea48694ad8f876961f3bd49632a
shivam <shivamvikhar5500@gmail.com>
2024-02-01 21:18:14 +0530

    Initial

---
+++ assets/js/api.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const api_key = "f23a01504d1d6a1ed7d169359e1ba2c4";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data))
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  /**
+   * @param {string} query Search query e.g.: "London" , "New York"
+   */
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
